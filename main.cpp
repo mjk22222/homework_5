8b2db1dfb8a1d8e0c568d78b8a17e974a28b7c72
mjk22222 <maxgerasimenko221@gmail.com>
2024-05-21 22:32:33 +0200

    added

---
+++ main.cpp
@@ -0,0 +1,14 @@
+﻿#include <SFML/Graphics.hpp>
+#include <Circle.hpp>
+#include <Game.hpp>
+// Задача 5 “Соударение шариков”
+
+int main()
+{
+    mt::Game game(1000, 600, "Game");
+    game.Setup(10);
+
+    game.LifeCycle();
+
+    return 0;
+}
